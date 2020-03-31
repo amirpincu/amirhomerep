e7e9635b0e23057f5038f99fb8622af03a102433
Amir <amir.pincu@gmail.com>
2020-03-31 13:49:19 +0300

    city weather component compass

---
+++ src/app/weather/models/city-weather.model.ts
@@ -0,0 +1,15 @@
+export enum CityWeatherDescription {
+    sunny = 0,
+    cloudy,
+    raining,
+    storm,
+    snowing,
+}
+
+export class CityWeatherData {
+    city: string;
+    temp: number;
+    maxTemp: number;
+    minTemp: number;
+    weatherDesc: string;
+}
--- src/app/weather/weather-comp/weather-comp.component.ts
+++ src/app/weather/weather-comp/weather-comp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { CityWeatherDescription, CityWeatherData } from '../models/city-weather.model';
 
 @Component({
   selector: 'app-weather-comp',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-comp.component.css']
 })
 export class WeatherCompComponent implements OnInit {
+  CityWeatherDescription = CityWeatherDescription;
+  @Input()
+  data: CityWeatherData = { city: `City not specified`, temp: 0, maxTemp: 1, minTemp: -1, weatherDesc: `not specified`};
+
+  public weatherImageSrc()
 
   constructor() { }
 
