432551bf2a3d5eca932d2cc0afc6b99936d7d23a
Amir <amir.pincu@gmail.com>
2020-04-02 19:12:07 +0300

    rest of code

--- src/app/weather/services/weather-serv.service.ts
+++ src/app/weather/services/weather-serv.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class WeatherServService {
 
   constructor(public http: HttpClient) { }
 
-  getCityWeatherByName(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<string> {
+  public getCityWeatherByName(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<string> {
     const dataSub = new Subject<string>();
     this.http.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
@@ -21,7 +21,30 @@ export class WeatherServService {
     return dataSub;
   }
 
-  private getMaxTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
+  // Returns weather description: clear, sunny, cloudy, raining, storm, snowy
+  public getWeatherState(city: string): Subject<string> {
+    const dataSubject = new Subject<string>();
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((data) => {
+        dataSubject.next(data['weather'][0].main);
+      });
+    return dataSubject;
+  }
+
+  // Returns current temperature
+  public getCurrentTemperature(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+    const dataSubject = new Subject<number>();
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((weather: any) => {
+        dataSubject.next(Math.round(Number(weather.main.temp)));
+      });
+    return dataSubject;
+  }
+
+  // Returns the maximum temperature
+  public getMaxTemperature(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
     const dataSubject = new Subject<number>();
     let max: number;
     this.http.get(
@@ -38,7 +61,8 @@ export class WeatherServService {
     return dataSubject;
   }
 
-  private getMinTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
+  // Returns the minimum temperature
+  public getMinTemperature(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
     const dataSubject = new Subject<number>();
     let min: number;
     this.http.get(
@@ -51,16 +75,6 @@ export class WeatherServService {
           }
         });
         dataSubject.next(Math.round(min));
-      });
-    return dataSubject;
-  }
-
-  private getForecast(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<Array<any>>  {
-    const dataSubject = new Subject<Array<any>>();
-    this.http.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
-      .subscribe((weather: any) => {
-        dataSubject.next(weather.list);
       });
     return dataSubject;
   }
--- src/app/weather/weather-view-comp/weather-view-comp.component.ts
+++ src/app/weather/weather-view-comp/weather-view-comp.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CityWeatherData, CityWeatherDescription } from '../models/city-weather.model';
+import { CityWeatherData, CityWeatherDescription as CityWeatherState } from '../models/city-weather.model';
+import { WeatherServService } from '../services/weather-serv.service';
+import { state } from '@angular/animations';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-weather-view-comp',
@@ -9,10 +12,58 @@ import { CityWeatherData, CityWeatherDescription } from '../models/city-weather.
 export class WeatherViewCompComponent implements OnInit {
   private cities: CityWeatherData[] = [];
 
-  constructor() { }
+  // City info
+  private cityName: string;
+  private cityState: CityWeatherState;
+  private cityTemp: number;
+  private cityMaxTemp: number;
+  private cityMinTemp: number;
+
+  // subscriptions
+  private stateSub: Subscription;
+  private tempSub: Subscription;
+  private maxTempSub: Subscription;
+  private minTempSub: Subscription;
+
+  constructor(private wethServ: WeatherServService) { }
 
   ngOnInit(): void {
-    this.cities.push({ city: 'new-york', temp: 15, maxTemp: 18, minTemp: 14, weatherDesc: CityWeatherDescription.raining });
+    this.cities.push({ city: 'new-york', temp: 15, maxTemp: 18, minTemp: 14, weatherDesc: CityWeatherState.raining });
   }
 
+  addCity(): void {
+    this.stateSub = this.wethServ.getWeatherState(this.cityName).subscribe((state) => this.cityState = this.stateStringtoEnum(state));
+    this.tempSub = this.wethServ.getCurrentTemperature(this.cityName).subscribe((temp) => this.cityTemp = temp);
+    this.maxTempSub = this.wethServ.getMaxTemperature(this.cityName).subscribe((temp) => this.cityMaxTemp = temp);
+    this.minTempSub = this.wethServ.getMinTemperature(this.cityName).subscribe((temp) => this.cityMinTemp = temp);
+
+    this.cities.push( { city: this.cityName, temp: this.cityTemp, maxTemp: this.cityMaxTemp, 
+      minTemp: this.cityMinTemp, weatherDesc: this.cityState } );
+  }
+
+  stateStringtoEnum(state: string) {
+    switch (state) {
+      case 'clear':
+        return CityWeatherState.clear;
+      case 'sunny':
+        return CityWeatherState.sunny;
+      case 'cloudy':
+        return CityWeatherState.cloudy;
+      case 'raining':
+        return CityWeatherState.raining;
+      case 'storm':
+        return CityWeatherState.storm;
+      case 'snowing':
+        return CityWeatherState.snowing;
+    }
+
+    return undefined;
+  }
+
+  ngOnDestroy() {
+    this.stateSub.unsubscribe();
+    this.tempSub.unsubscribe();
+    this.maxTempSub.unsubscribe();
+    this.minTempSub.unsubscribe();
+  }
 }
