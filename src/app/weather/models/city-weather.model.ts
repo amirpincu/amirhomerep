a5816e54995befee74a2e4b07e4b78744a68145f
Amir <amir.pincu@gmail.com>
2020-04-02 15:03:23 +0300

    update

--- src/app/weather/models/city-weather.model.ts
+++ src/app/weather/models/city-weather.model.ts
@@ -4,6 +4,7 @@ export enum CityWeatherDescription {
     raining,
     storm,
     snowing,
+    clear,
 }
 
 export class CityWeatherData {
--- src/app/weather/services/weather-serv.service.ts
+++ src/app/weather/services/weather-serv.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getCityWeatherByName(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<string> {
+    const dataSub = new Subject<string>();
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((data) => {
+        dataSub.next(data['weather']);
+      }, (err) => {
+        console.log(err);
+      });
+    return dataSub;
+  }
+
+  private getMaxTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
+    const dataSubject = new Subject<number>();
+    let max: number;
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((weather: any) => {
+        max = weather.list[0].main.temp;
+        weather.list.forEach((value) => {
+          if (max < value.main.temp) {
+            max = value.main.temp;
+          }
+        });
+        dataSubject.next(Math.round(max));
+      });
+    return dataSubject;
+  }
+
+  private getMinTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number>  {
+    const dataSubject = new Subject<number>();
+    let min: number;
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((weather: any) => {
+        min = weather.list[0].main.temp;
+        weather.list.forEach((value) => {
+          if (min > value.main.temp) {
+            min = value.main.temp;
+          }
+        });
+        dataSubject.next(Math.round(min));
+      });
+    return dataSubject;
+  }
+
+  private getForecast(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<Array<any>>  {
+    const dataSubject = new Subject<Array<any>>();
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${metric}&APPID=952d6b1a52fe15a7b901720074680562`)
+      .subscribe((weather: any) => {
+        dataSubject.next(weather.list);
+      });
+    return dataSubject;
+  }
 }
